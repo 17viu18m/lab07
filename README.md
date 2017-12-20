b2e3cdbf751a5baffb01846636f5bbeb474d2ced
17viu18m <v2017.17viu18m@outlook.com>
2017-12-20 19:32:15 +0300

    added documentation

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Build Status](https://travis-ci.org/17viu18m/lab07.svg?branch=master)](https://travis-ci.org/17viu18m/lab07)
--- include/print.hpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <fstream>
-#include <iostream>
-
-void print(const std::string& text, std::ostream& out = std::cout);  	# Print1
-void print(const std::string& text, std::ofstream& out);		# Print2	
